a0e5abb234598cc6c032e064e50a0728a1b7712d
fugeretrevin <fugeretrevin@gmail.com>
2025-05-10 12:40:50 -0500

    Added time based greeting

--- scripts.js
+++ scripts.js
@@ -5,3 +5,20 @@ function playSound(sound)
 {
     sound.play();
 }
+
+const greetingDiv = document.getElementById('greeting-text');
+const hour = new Date().getHours();
+
+let greeting;
+
+if (hour >= 4 && hour < 12) {
+    greeting = "Good Morning,";
+}
+else if (hour >= 12 && hour < 17) {
+    greeting = "Good Afternoon,";
+}
+else {
+    greeting = "Good Evening,";
+}
+
+greetingDiv.textContent = greeting;
